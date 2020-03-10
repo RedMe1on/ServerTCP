0793efc724619189da5285bac8496977969a92ba
RedMe1on <lalkogad@gmail.com>
2020-03-10 17:48:44 +0300

    Initial client.py
    Simple client socket created

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('127.0.0.1', 53210))
+client_socket.sendall(b'Hey, everyone, get me some chips!')
+
+data = client_socket.recv(1024)
+client_socket.close()
+print('Received', repr(data))
